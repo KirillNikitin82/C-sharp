78385bb17ad678197e4625b4f6370b044a7ed9a8
Kirill <nikitinkirill82@yandex.ru>
2023-04-08 01:22:21 +0300

    Added prog Task_001

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -5,5 +5,15 @@
 // a = 2, b = 10 -> нет
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
-Console.WriteLine ("Введите первое число");
-
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
