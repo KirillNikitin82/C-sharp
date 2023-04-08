fddca2a9ffb69c97dda9171ae2934222a39f69ab
Kirill <nikitinkirill82@yandex.ru>
2023-04-08 20:37:00 +0300

    Added prog Task_003

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -2,4 +2,38 @@
 // название дня недели по заданному номеру.
 // 3 -> Среда
 // 5 -> Пятница
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите номер дня недели");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (num == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (num == 7)
+{
+    Console.WriteLine("Воскресение");
+}
+else if ((num > 7)|(num < 1))
+{
+    Console.WriteLine("Нет такого дня недели");
+}
