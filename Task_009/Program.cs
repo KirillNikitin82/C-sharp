0cb615b58552d71d5200d4861810389324ac3da2
Kirill <nikitinkirill82@yandex.ru>
2023-04-11 15:37:57 +0300

    Added Practice2

---
+++ Task_009/Program.cs
@@ -0,0 +1,33 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100); //99+1
+Console.WriteLine($"Случайное число из отрезка 10-99 -> {number}");
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit > secondDigit) 
+// {
+// Console.WriteLine($"Наибольшая цифра цисла -> {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine($"Наибольшая цифра цисла -> {secondDigit}");
+// }
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра цисла -> {maxDigit}");
+
+int maxDigit = MaxDigit (number);
+Console.WriteLine($"Наибольшая цифра цисла -> {maxDigit}");
+
+int MaxDigit (int num) // num = number если использовать глобальную переменную смысл метода теряется, программа создает одно и тоже число.
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return maxDigit;
+}
---
+++ Task_011/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000); 
+Console.WriteLine($"Случайное число -> {number}");
+
+int result = DeleteSecondDigit (number);
+Console.WriteLine($"Удаляем вторую цифру числа -> {result}");
+
+int DeleteSecondDigit (int num) 
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int res = firstDigit*10 + thirdDigit;
+    return res;
+}
