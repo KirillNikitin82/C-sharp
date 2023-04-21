4a6d3d96fc9ebed1f5365f2630d57bd89e569d1d
Kirill <nikitinkirill82@yandex.ru>
2023-04-21 17:01:46 +0300

    Added T_29,T_27,T_025

---
+++ Task_025/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = Exponent( numberA, numberB, array);
+int[] arr = new int[numberB];
+
+// int[] arr1 = PrintArray(arr[], numberB, numberA);
+// System.Console.WriteLine();
+
+
+// int[] CreateArray(int numB, int numA)
+// {
+//     int[] array = new int[numB];
+//     for (int i = 0; i <= numB; i++)
+//     {
+//         array[i] = numA;
+//     }
+//     return array;
+// }
+
+int PrintArray(int[] array, int numB, int numA)
+{
+    int i = 0;
+    while (i < numB)
+    {
+        array[i] = numA;
+        Console.Write($"{array[i]} ");
+    }
+    return array[i];
+}
+
+// int Exponent(int numA, int numB, int[] arr)
+// {
+//     int sum = 0;
+//     int count = 0;
+//     while (count <= numB)
+//     {
+//         sum = numA * arr[count];
+//         count++;
+//     }
+//     return sum;
+// }
