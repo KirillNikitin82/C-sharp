315210ccd4d44aae638f392b6225cccfa921da87
Kirill <Nikitnkirill82@yandex.ru>
2023-04-23 21:45:21 +0300

    Added prog T_034, T_036, T_038

--- Task_036/Program.cs
+++ Task_036/Program.cs
@@ -4,3 +4,42 @@
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] array = CreateArrayRndInt(4, -10, 100);
+Console.Write("[");
+PrintArray(array);
+Console.Write("] ->");
+
+int sumOddElements = SumOddElements(array);
+System.Console.WriteLine($"  {sumOddElements}");
+
+int SumOddElements(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+            sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
--- Task_038/Program.cs
+++ Task_038/Program.cs
@@ -4,13 +4,38 @@
 
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
-doble[] CreateArrayRndDouble(int size, int min, int max)
+double[] array = CreateArrayRndDouble(5, 1, 100);
+Console.Write("[");
+PrintArray(array);
+Console.Write("] ->");
+
+double result = DefferenceMaxMin(array);
+double resultRound = Math.Round(result, 1);
+System.Console.WriteLine($"  {resultRound}");
+
+double DefferenceMaxMin(double[] arr)
+{
+double max = arr[0];
+double min = arr[0];
+for (int i = 1; i < arr.Length; i++)
+{
+        if (max < arr[i])
+        max = arr[i];
+        if (min>arr[i])
+        min = arr[i];
+}
+double result = max - min;
+return result;
+}
+
+double[] CreateArrayRndDouble(int size, int min, int max)
 {
     double[] arr = new double[size];
     Random rnd = new Random();
+
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = rnd.NextDouble()*(max-min)+max;
+        arr[i] = rnd.NextDouble() * (max - min) + min;
     }
     return arr;
 }
