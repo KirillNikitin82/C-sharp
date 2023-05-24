9bd61829a5ce78f65a90e0ebb38a1810c66f3389
Kirill <Nikitnkirill82@yandex.ru>
2023-05-24 23:46:24 +0300

    Added last HomeWork

---
+++ Task_066/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите целое натуральное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое натуральное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM > 0 && numberN > 0)
+{
+    int sumElements = SumElements(numberM, numberN);
+    Console.WriteLine($"Сумма чисел от {numberM} до {numberN} = {sumElements}");
+}
+else
+{
+    Console.WriteLine("Ввод неверного значения");
+    return;
+}
+
+int SumElements(int numA, int numB)
+{
+    int sum = 0;
+    int temp = 0;
+    if (numA < numB)
+    {
+        sum = numA + SumElements(numA + 1, numB);
+    }
+    else if (numA > numB)
+    {
+        temp = numA;
+        numA = numB;
+        numB = temp;
+        sum = numA + SumElements(numA + 1, numB);
+    }
+    else sum = sum + numB;
+    return sum;
+}
---
+++ Task_068/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите целое натуральное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое натуральное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM >= 0 && numberN >= 0)
+{
+    int accermanFunction = AccermanFunction(numberM, numberN);
+    System.Console.WriteLine($"Значение функции Аккермана = {accermanFunction}");
+}
+else
+{
+    Console.WriteLine("Введите корректные значиния");
+    return;
+}
+
+int AccermanFunction(int numM, int numN)
+{
+    if (numM == 0)
+        return numN + 1;
+    else if ((numM != 0) && (numN == 0))
+        return AccermanFunction(numM - 1, 1);
+    else
+        return AccermanFunction(numM - 1, AccermanFunction(numM, numN - 1));
+}
